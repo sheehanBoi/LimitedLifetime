035512848f3ec1ac8fd1615e2c23515b95364e55
Sheehan <85948819+sheehanBoi@users.noreply.github.com>
2025-07-21 20:40:04 +0100

    add tilemaps and sprites and ui for player health

--- Assets/_Scripts/GameOver.cs
+++ Assets/_Scripts/GameOver.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
+    public GameObject winTextUI;
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Debug.Log("You Win! Game Over!");
+
+            Time.timeScale = 0f;
+
+            if (winTextUI != null) 
+            {
+                winTextUI.SetActive(true);
+            }
         }
     }
 }
--- Assets/_Scripts/PlayerHealth.cs
+++ Assets/_Scripts/PlayerHealth.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float health = 7;
-    
+    public float health = 50f;
+    public float maxHealth = 50f;
+    public Slider healthSlider;
+
+    private void Start()
+    {
+        if(healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = health;
+        }
+    }
+
     public void TakeDamage(float damage)
     {
         health -= damage;
+
+        if(healthSlider != null)
+        {
+            healthSlider.value = health;
+        }
+
         if(health <= 0)
         {
             Die();
